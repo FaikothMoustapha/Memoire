c67698869c95483e54ad5121c2cf6deaf5ef4c38
FaikothMoustapha <faikothmoustapha0@gmail.com>
2025-05-09 13:03:19 +0100

    Nous avançons1

--- jofa.sql
+++ jofa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 07 mai 2025 à 19:31
+-- Généré le : ven. 09 mai 2025 à 14:02
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -37,6 +37,109 @@ CREATE TABLE `activites` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `activites`
+--
+
+INSERT INTO `activites` (`id`, `libAct`, `datePrev`, `dateFinAct`, `statut`, `etape_id`, `created_at`, `updated_at`) VALUES
+(1, 'Identification des objectifs et des besoins spécifiques', NULL, NULL, 'Début', 1, NULL, NULL),
+(2, 'Définition des critères de réussite et des indicateurs de performance', NULL, NULL, 'Début', 1, NULL, NULL),
+(3, 'Validation des ressources nécessaires', NULL, NULL, 'Début', 1, NULL, NULL),
+(4, 'Analyse détaillée des fonctionnalités', NULL, NULL, 'Début', 2, NULL, NULL),
+(5, 'Rédaction des spécifications techniques et fonctionnelles', NULL, NULL, 'Début', 2, NULL, NULL),
+(6, 'Validation des spécifications avec les parties prenantes', NULL, NULL, 'Début', 2, NULL, '2025-05-09 08:29:29'),
+(7, 'Programmation des fonctionnalités conformément aux spécifications', NULL, NULL, 'Début', 3, NULL, NULL),
+(8, 'Tests unitaires des modules développés', NULL, NULL, 'Début', 3, NULL, NULL),
+(9, 'Intégration des différents composants du service', NULL, NULL, 'Début', 3, NULL, NULL),
+(10, 'Préparation de l\'environnement de production', NULL, NULL, 'Début', 4, NULL, NULL),
+(11, 'Installation et configuration', NULL, NULL, 'Début', 4, NULL, NULL),
+(12, 'Vérification de la disponibilité et de la stabilité en production', NULL, NULL, 'Début', 4, NULL, NULL),
+(13, 'Communication', NULL, NULL, 'Début', 5, NULL, NULL),
+(14, 'Formation aux utilisateurs', NULL, NULL, 'Début', 5, NULL, NULL),
+(15, 'évaluation continue de l\'adoption du changement', NULL, NULL, 'Début', 5, NULL, NULL),
+(16, 'Accompagnement', NULL, NULL, 'Début', 5, NULL, NULL),
+(17, 'collecte de feedbacks', NULL, NULL, 'Début', 5, NULL, NULL),
+(18, 'ajustements nécessaires', NULL, NULL, 'Début', 5, NULL, NULL),
+(19, 'collecte des données', NULL, NULL, 'Début', 6, NULL, NULL),
+(20, 'identification des besoins', NULL, NULL, 'Début', 6, NULL, '0000-00-00 00:00:00'),
+(21, 'validation du rapport d’état des lieux', NULL, NULL, 'Début', 6, NULL, NULL),
+(22, 'définition de l’architecture logicielle', NULL, NULL, 'Début', 7, NULL, NULL),
+(23, 'mise en place des interfaces utilisateurs', NULL, NULL, 'Début', 7, NULL, NULL),
+(24, 'implémentation des fonctionnalités', NULL, NULL, 'Début', 7, NULL, NULL),
+(25, 'définition des scénarios', NULL, NULL, 'Début', 8, NULL, NULL),
+(26, 'exécution des scénarios', NULL, NULL, 'Début', 8, NULL, NULL),
+(27, 'rapport de test', NULL, NULL, 'Début', 8, NULL, NULL),
+(28, 'corrections des problèmes détectés lors des tests', NULL, NULL, 'Début', 9, NULL, NULL),
+(29, 'préparation de l’environnement d’hébergement', NULL, NULL, 'Début', 10, NULL, NULL),
+(30, 'installation', NULL, NULL, 'Début', 9, NULL, NULL),
+(31, 'formation', NULL, NULL, 'Début', 11, NULL, NULL),
+(32, 'surveillance des performances', NULL, NULL, 'Début', 12, NULL, NULL),
+(33, 'correction des bugs', NULL, NULL, 'Début', 12, NULL, NULL),
+(34, 'Communication', NULL, NULL, 'Début', 13, NULL, NULL),
+(35, 'Formation aux utilisateurs', NULL, NULL, 'Début', 13, NULL, NULL),
+(36, 'Accompagnement', NULL, NULL, 'Début', 13, NULL, NULL),
+(37, 'évaluation continue de l\'adoption du changement', NULL, NULL, 'Début', 13, NULL, NULL),
+(38, 'collecte de feedbacks', NULL, NULL, 'Début', 13, NULL, NULL),
+(39, 'ajustements nécessaires', NULL, NULL, 'Début', 13, NULL, NULL),
+(40, 'Analyse des exigences du système', NULL, NULL, 'Début', 14, NULL, '0000-00-00 00:00:00'),
+(41, 'Planification des ressources nécessaires', NULL, NULL, 'Début', 14, NULL, NULL),
+(42, 'Définition de l\'architecture de l\'infrastructure', NULL, NULL, 'Début', 15, NULL, NULL),
+(43, 'Mise en place des composants matériels et logiciels nécessaires', NULL, NULL, 'Début', 15, NULL, NULL),
+(44, 'Élaboration des schémas de conception', NULL, NULL, 'Début', 15, NULL, NULL),
+(45, 'Identification des équipements nécessaires', NULL, NULL, 'Début', 16, NULL, NULL),
+(46, 'Sélection des fournisseurs et des solutions logicielles', NULL, NULL, 'Début', 16, NULL, NULL),
+(47, 'Acquisition des équipements et logiciels', NULL, NULL, 'Début', 16, NULL, NULL),
+(48, 'Installation des équipements et logiciels', NULL, NULL, 'Début', 17, NULL, NULL),
+(49, 'Configuration des composants pour assurer une intégration optimale', NULL, NULL, 'Début', 17, NULL, NULL),
+(50, 'Vérification de la compatibilité entre les différents éléments', NULL, NULL, 'Début', 17, NULL, NULL),
+(51, 'Définition des scénarios de test pour l\'infrastructure', NULL, NULL, 'Début', 18, NULL, NULL),
+(52, 'Exécution des tests de fonctionnement', NULL, NULL, 'Début', 18, NULL, NULL),
+(53, 'Rapport de test et correction des anomalies détectées', NULL, NULL, 'Début', 18, NULL, NULL),
+(54, 'Mise en place de programmes de formation pour les utilisateurs et les administrateurs', NULL, NULL, 'Début', 19, NULL, NULL),
+(55, 'Création de documentation technique et d\'utilisation', NULL, NULL, 'Début', 19, NULL, NULL),
+(56, 'Formation du personnel sur l\'utilisation et la maintenance de l\'infrastructure', NULL, NULL, 'Début', 19, NULL, NULL),
+(57, 'Communication', NULL, NULL, 'Début', 20, NULL, NULL),
+(58, 'Accompagnement', NULL, NULL, 'Début', 20, NULL, NULL),
+(59, 'évaluation continue de l\'adoption du changement', NULL, NULL, 'Début', 20, NULL, NULL),
+(60, 'collecte de feedbacks', NULL, NULL, 'Début', 20, NULL, '0000-00-00 00:00:00'),
+(61, 'ajustements nécessaires', NULL, NULL, 'Début', 13, NULL, NULL),
+(62, 'Analyse des exigences du système', NULL, NULL, 'Début', 14, NULL, NULL),
+(63, 'Planification des ressources nécessaires', NULL, NULL, 'Début', 14, NULL, NULL),
+(64, 'Définition de l\'architecture de l\'infrastructure', NULL, NULL, 'Début', 15, NULL, NULL),
+(65, 'Mise en place des composants matériels et logiciels nécessaires', NULL, NULL, 'Début', 15, NULL, NULL),
+(66, 'Élaboration des schémas de conception', NULL, NULL, 'Début', 15, NULL, NULL),
+(67, 'Identification des équipements nécessaires', NULL, NULL, 'Début', 16, NULL, NULL),
+(68, 'Sélection des fournisseurs et des solutions logicielles', NULL, NULL, 'Début', 16, NULL, NULL),
+(69, 'Acquisition des équipements et logiciels', NULL, NULL, 'Début', 15, NULL, NULL),
+(70, 'Installation des équipements et logiciels', NULL, NULL, 'Début', 17, NULL, NULL),
+(71, 'Configuration des composants pour assurer une intégration optimale', NULL, NULL, 'Début', 17, NULL, NULL),
+(72, 'Vérification de la compatibilité entre les différents éléments', NULL, NULL, 'Début', 17, NULL, NULL),
+(73, 'Définition des scénarios de test pour l\'infrastructure', NULL, NULL, 'Début', 18, NULL, NULL),
+(74, 'Exécution des tests de fonctionnement', NULL, NULL, 'Début', 18, NULL, NULL),
+(75, 'Rapport de test et correction des anomalies détectées', NULL, NULL, 'Début', 18, NULL, NULL),
+(76, 'Mise en place de programmes de formation pour les utilisateurs et les administrateurs', NULL, NULL, 'Début', 19, NULL, NULL),
+(77, 'Création de documentation technique et d\'utilisation', NULL, NULL, 'Début', 19, NULL, NULL),
+(78, 'Formation du personnel sur l\'utilisation et la maintenance de l\'infrastructure', NULL, NULL, 'Début', 19, NULL, NULL),
+(79, 'Communication', NULL, NULL, 'Début', 20, NULL, NULL),
+(80, 'Accompagnement', NULL, NULL, 'Début', 20, NULL, NULL),
+(81, 'évaluation continue de l\'adoption du changement', NULL, NULL, 'Début', 20, NULL, NULL),
+(82, 'collecte de feedbacks', NULL, NULL, 'Début', 20, NULL, NULL),
+(83, 'ajustements nécessaires', NULL, NULL, 'Début', 20, NULL, NULL),
+(84, 'Recherche exhaustive et analyse critique des travaux existants dans le domaine d\'étude', NULL, NULL, 'Début', 21, NULL, NULL),
+(85, 'Synthèse des connaissances actuelles et identification des lacunes', NULL, NULL, 'Début', 21, NULL, NULL),
+(86, 'Évaluation des Forces', NULL, NULL, 'Début', 22, NULL, NULL),
+(87, 'Faiblesses', NULL, NULL, 'Début', 22, NULL, NULL),
+(88, 'Opportunités et Menaces liées à l\'objet d\'étude', NULL, NULL, 'Début', 22, NULL, NULL),
+(89, 'Analyse de risques', NULL, NULL, 'Début', 22, NULL, NULL),
+(90, 'Développement de différents scénarios possibles en fonction des résultats de l\'analyse SWOT', NULL, NULL, 'Début', 23, NULL, NULL),
+(91, 'Modélisation des impacts potentiels de chaque scénario sur le projet', NULL, NULL, 'Début', 23, NULL, NULL),
+(92, 'Formulation d\'un plan d\'action détaillé basé sur les scénarios envisagés', NULL, NULL, 'Début', 24, NULL, NULL),
+(93, 'Définition des indicateurs de suivi', NULL, NULL, 'Début', 24, NULL, NULL),
+(94, 'Assignation des responsabilités et des ressources nécessaires à chaque étape du plan', NULL, NULL, 'Début', 24, NULL, NULL),
+(95, 'Implémentation des actions définies dans le plan d\'action', NULL, NULL, 'Début', 25, NULL, NULL),
+(96, 'Surveillance continue des progrès réalisés', NULL, NULL, 'Début', 25, NULL, NULL),
+(97, 'Ajustement du plan en fonction des résultats obtenus', NULL, NULL, 'Début', 25, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -75,6 +178,16 @@ CREATE TABLE `categories` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `libCat`, `created_at`, `updated_at`) VALUES
+(1, 'E-services ', NULL, NULL),
+(2, 'Développement logiciel ', NULL, NULL),
+(3, 'Infrastructure numérique ', NULL, NULL),
+(4, 'Etudes', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -104,6 +217,37 @@ CREATE TABLE `etapes` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `etapes`
+--
+
+INSERT INTO `etapes` (`id`, `libEtape`, `categorie_id`, `created_at`, `updated_at`) VALUES
+(1, 'Initialisation', 1, NULL, NULL),
+(2, 'Spécification', 1, NULL, NULL),
+(3, 'Développement', 1, NULL, NULL),
+(4, 'Exploitation', 1, NULL, NULL),
+(5, 'Conduite du changement ', 1, NULL, NULL),
+(6, 'Analyse des besoins ', 2, NULL, NULL),
+(7, 'Conception', 2, NULL, NULL),
+(8, 'Tests ', 2, NULL, NULL),
+(9, 'Intégration ', 2, NULL, NULL),
+(10, 'Exploitation ', 2, NULL, NULL),
+(11, 'Formation ', 2, NULL, NULL),
+(12, 'Maintenance ', 2, NULL, NULL),
+(13, 'Conduite du changement ', 2, NULL, NULL),
+(14, 'Évaluation des besoins et planification ', 3, NULL, NULL),
+(15, 'Conception de l’infrastructure ', 3, NULL, NULL),
+(16, 'Acquisitions des équipements et logiciels ', 3, NULL, NULL),
+(17, 'Intégration/Exploitation ', 3, NULL, NULL),
+(18, 'Test de fonctionnement ', 3, NULL, NULL),
+(19, 'Formation et documentation ', 3, NULL, NULL),
+(20, 'Conduite du changement ', 3, NULL, NULL),
+(21, 'Revues de la littérature ', 4, NULL, NULL),
+(22, 'Analyse SWOT ', 4, NULL, NULL),
+(23, 'Élaboration de scénarios ', 4, NULL, NULL),
+(24, 'Élaboration du plan d\'action ', 4, NULL, NULL),
+(25, 'Mise en œuvre et suivi ', 4, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -237,7 +381,7 @@ CREATE TABLE `prestataires` (
 --
 
 INSERT INTO `prestataires` (`id`, `code`, `nomStructure`, `nomResponsable`, `email`, `telephone`, `created_at`, `updated_at`) VALUES
-(1, 'da112', 'WXCVBN?./§§/.?NB', 'KJHGFDSDFGHJKLM', 'se@gmail.com', '1234567890234567890', '2025-05-07 15:45:03', '2025-05-07 15:45:03');
+(1, 'darrrr', 'WXCVBN?./§§/.?NB', 'KJHGFDSDFGHJKLM', 'se@gmail.com', '1234567890234567890', '2025-05-07 15:45:03', '2025-05-09 10:21:10');
 
 -- --------------------------------------------------------
 
@@ -342,7 +486,10 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('RYyy6LKCf7yyRIGkmZG1Gf4IG6jW4zBFGzuZSizj', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiT0c5NWJ5Tng1S0htb3h1RFo4Q2VsRWVQRmpUUDFnYTlqQ2kyNGxKTCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzk6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9saXN0L3ByZXN0YXRhaXJlcyI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1746638242);
+('FEkm2rVyXIAcEo7KGgZeZYGhT7eG0hON3EcKSQ6z', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoibmtJazB1cUNva3d6bjVpN0g5Q3VNY3BjQ3Q1TEZTQUNBenpNQjNNYSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9kaXJlY3RldXIvZGFzaGJvYXJkIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mjt9', 1746790963),
+('Gad6xqMU9Ediy6b8pDGUIgVvhwqxVaqSoQwQS61w', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiZFBSdjZXTlZlNFBadDliSnN0VHNmejJzd0dYcnlOZFF4NWlOMDdCUCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9kaXJlY3RldXIvZGFzaGJvYXJkIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mjt9', 1746791808),
+('M6mW2p4MZZVZmdNyEC0pu0yspO62InhrLSteJMJd', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36', 'YToyOntzOjY6Il90b2tlbiI7czo0MDoieVg4bFVvMUtub3kxTHIzSUZCekhBQWRITm45dmV3b2RhSUlhM3dzViI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1746791150),
+('sTDJ6TZ61gjEjUmnSjgclI83xZTN3i3OBoUnVyh4', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoibDRIcmxVelN0bmtiVEFHVzRqWmdJWVhZNGNiem8wd1N3WU1zNzRKNCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9kaXJlY3RldXIvZGFzaGJvYXJkIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mjt9', 1746791207);
 
 -- --------------------------------------------------------
 
@@ -589,13 +736,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `activites`
 --
 ALTER TABLE `activites`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 
 --
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `documents`
@@ -607,7 +754,7 @@ ALTER TABLE `documents`
 -- AUTO_INCREMENT pour la table `etapes`
 --
 ALTER TABLE `etapes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT pour la table `failed_jobs`
@@ -637,7 +784,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT pour la table `prestataires`
 --
 ALTER TABLE `prestataires`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `programmes`
